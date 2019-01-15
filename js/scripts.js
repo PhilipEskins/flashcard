d2c25cf2346d1a3033b8e8596548cf5e9d11f54d
Fen Lombardi and Philip Eskins <fen+philip@>
2019-01-15 15:34:44 -0800

    add additional definitions

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,34 @@ $(document).ready(function() {
   $("#OperatorsLink").click(function() {
     $("#Operators").slideToggle();
   });
+
+  $("#VariablesLink").click(function() {
+    $("#Variables").slideToggle();
+  });
+
+  $("#NamingLink").click(function() {
+    $("#Naming").slideToggle();
+  });
+
+  $("#FunctionsLink").click(function() {
+    $("#Functions").slideToggle();
+  });
+
+  $("#MethodsLink").click(function() {
+    $("#Methods").slideToggle();
+  });
+
+  $("#ArgumentsLink").click(function() {
+    $("#Arguments").slideToggle();
+  });
+
+  $("#ParametersLink").click(function() {
+    $("#Parameters").slideToggle();
+  });
+
+  $("#ReturnsLink").click(function() {
+    $("#Returns").slideToggle();
+  });
+
+
 });
