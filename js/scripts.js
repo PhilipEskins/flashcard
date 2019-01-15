2eaa27885d7dca3d17299be6f9bb331a0f5487bf
Fen Lombardi and Philip Eskins <fen@>
2019-01-15 14:17:12 -0800

    add second example

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
   });
 
   $(".clickable").click(function() {
-    $("#hidecontent").slideToggle();
+    $("#Example").remove();
+    $(".hidecontent").slideToggle();
   });
 });
